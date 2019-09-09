7b101aa2f0c54e7bc80931dac66530ea3191845a
Jianrui Hu <u0886750@utah.edum>
2019-09-09 13:31:26 -0600

    Map update

--- app/src/main/java/com/example/gongtia/lifestyle/HomeActivity.java
+++ app/src/main/java/com/example/gongtia/lifestyle/HomeActivity.java
@@ -1,16 +1,36 @@
 package com.example.gongtia.lifestyle;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.view.MenuItem;
 
-public class HomeActivity extends AppCompatActivity {
+import android.provider.Settings;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity
+        implements View.OnClickListener {
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+    double latitude;
+    double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +39,62 @@ public class HomeActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        Button mButton = findViewById(R.id.button);
+        mButton.setOnClickListener(this);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_SOUND_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET
+                },10);
+                return;
+            }else{
+                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+            }
+        }
     }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        switch (requestCode){
+//            case 10:
+//                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+//                    configureButton();
+//                return;
+//        }
+//    }
+//
+//    private void configureButton() {
+//
+//        locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+//    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
@@ -45,4 +120,24 @@ public class HomeActivity extends AppCompatActivity {
             return true;
         }
     };
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.button: {
+
+                //We have to grab the search term and construct a URI object from it.
+                //We'll hardcode WEB's location here
+                Uri searchUri = Uri.parse("geo:" + latitude + "," + longitude + "?q=" + "hike");
+
+                //Create the implicit intent
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, searchUri);
+
+                //If there's an activity associated with this intent, launch it
+                if(mapIntent.resolveActivity(getPackageManager())!=null){
+                    startActivity(mapIntent);
+                }
+            }
+        }
+    }
 }
