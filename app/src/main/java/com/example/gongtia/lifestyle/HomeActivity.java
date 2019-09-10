669c9c61100af1ffb06abb25142af5c018088180
Jianrui Hu <u0886750@utah.edum>
2019-09-09 18:53:32 -0600

    map fragment update ;

--- app/src/main/java/com/example/gongtia/lifestyle/HomeActivity.java
+++ app/src/main/java/com/example/gongtia/lifestyle/HomeActivity.java
@@ -3,9 +3,7 @@ package com.example.gongtia.lifestyle;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -25,12 +23,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity
-        implements View.OnClickListener {
-
-    LocationManager locationManager;
-    LocationListener locationListener;
-    double latitude;
-    double longitude;
+         {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,46 +32,6 @@ public class HomeActivity extends AppCompatActivity
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
-
-        Button mButton = findViewById(R.id.button);
-        mButton.setOnClickListener(this);
-
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                latitude = location.getLatitude();
-                longitude = location.getLongitude();
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-                Intent intent = new Intent(Settings.ACTION_SOUND_SETTINGS);
-                startActivity(intent);
-            }
-        };
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{
-                        Manifest.permission.ACCESS_FINE_LOCATION,
-                        Manifest.permission.ACCESS_COARSE_LOCATION,
-                        Manifest.permission.INTERNET
-                },10);
-                return;
-            }else{
-                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
-            }
-        }
     }
 
 //    @Override
@@ -96,48 +49,28 @@ public class HomeActivity extends AppCompatActivity
 //        locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
 //    }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            Fragment selectedFragment = null;
+         private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+             @Override
+             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                 Fragment selectedFragment = null;
 
-            switch (menuItem.getItemId()){
-                case R.id.nav_Home:
-                    Intent homeIntent = new Intent(HomeActivity.this, HomeActivity.class);
-                    startActivity(homeIntent);
-                    break;
+                 switch (menuItem.getItemId()){
+                     case R.id.nav_Home:
+                         Intent homeIntent = new Intent(HomeActivity.this, HomeActivity.class);
+                         startActivity(homeIntent);
+                         break;
 
-                case R.id.nav_goal:
-                    selectedFragment = new GoalFragment();
-                    break;
+                     case R.id.nav_goal:
+                         selectedFragment = new GoalFragment();
+                         break;
 
-                case R.id.nav_profile:
-                    selectedFragment = new ProfileFragment();
-                    break;
-            }
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    selectedFragment).commit();
-            return true;
-        }
-    };
-
-    @Override
-    public void onClick(View view) {
-        switch(view.getId()){
-            case R.id.button: {
-
-                //We have to grab the search term and construct a URI object from it.
-                //We'll hardcode WEB's location here
-                Uri searchUri = Uri.parse("geo:" + latitude + "," + longitude + "?q=" + "hike");
-
-                //Create the implicit intent
-                Intent mapIntent = new Intent(Intent.ACTION_VIEW, searchUri);
-
-                //If there's an activity associated with this intent, launch it
-                if(mapIntent.resolveActivity(getPackageManager())!=null){
-                    startActivity(mapIntent);
-                }
-            }
-        }
-    }
+                     case R.id.nav_profile:
+                         selectedFragment = new ProfileFragment();
+                         break;
+                 }
+                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                         selectedFragment).commit();
+                 return true;
+             }
+         };
 }
---
+++ app/src/main/java/com/example/gongtia/lifestyle/MapFragment.java
@@ -0,0 +1,186 @@
+package com.example.gongtia.lifestyle;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import android.provider.Settings;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link MapFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link MapFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MapFragment extends Fragment
+        implements View.OnClickListener {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+    double latitude;
+    double longitude;
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MapFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MapFragment newInstance(String param1, String param2) {
+        MapFragment fragment = new MapFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View view = inflater.inflate(R.layout.fragment_map, container, false);
+        Button mButton = view.findViewById(R.id.map_button);
+        mButton.setOnClickListener(this);
+
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_SOUND_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET
+                },10);
+                return view;
+            }else{
+                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+            }
+        }
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.map_button: {
+
+                //We have to grab the search term and construct a URI object from it.
+                //We'll hardcode WEB's location here
+                Uri searchUri = Uri.parse("geo:" + latitude + "," + longitude + "?q=" + "hike");
+
+                //Create the implicit intent
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, searchUri);
+
+                //If there's an activity associated with this intent, launch it
+                if(mapIntent.resolveActivity(getActivity().getPackageManager())!=null){
+                    startActivity(mapIntent);
+                }
+            }
+        }
+    }
+}
