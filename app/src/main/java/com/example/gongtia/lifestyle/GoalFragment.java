46168a7100972d5dd43729d73d40e0aef2e5c99c
Jianrui Hu <u0886750@utah.edum>
2019-09-15 22:07:54 -0600

    goal update

--- app/src/main/java/com/example/gongtia/lifestyle/GoalFragment.java
+++ app/src/main/java/com/example/gongtia/lifestyle/GoalFragment.java
@@ -7,11 +7,44 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class GoalFragment extends Fragment {
+
+    private Spinner spinner;
+    private Integer weight = 60;
+    private Integer height = 170;
+    private Boolean isMale = true;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_goal, container, false);
+        final View view = inflater.inflate(R.layout.fragment_goal, container, false);
+        //text view
+
+
+
+        // spinner
+        spinner = view.findViewById(R.id.spinner_active);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.active, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String text = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        return view;
     }
 }
+
